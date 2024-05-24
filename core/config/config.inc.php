e1325e9fa29f57e4fb06b99119994316e1e42642
Grassdor <rrr345@mail.ru>
2024-05-24 16:49:56 +0300

    add products

--- core/config/config.inc.php
+++ core/config/config.inc.php
@@ -5,15 +5,15 @@
  */
 $database_type = "mysql";
 $database_server = "localhost";
-$database_user = "root";
-$database_password = "";
+$database_user = "modxlocal";
+$database_password = "modxlocal";
 $database_connection_charset = 'utf8mb4';
-$dbase = "dk-lok";
+$dbase = "dklok";
 $table_prefix = 'modx_';
 $database_dsn = "$database_type:host=$database_server;dbname=$dbase;charset=utf8mb4";
-$config_options = array (
+$config_options = array(
 );
-$driver_options = array (
+$driver_options = array(
 );
 
 $lastInstallTime = 1715002473;
@@ -24,28 +24,28 @@ $https_port = '443';
 $uuid = '697831c0-005d-4aca-b0cd-797468a2ea50';
 
 if (!defined('MODX_CORE_PATH')) {
-    $modx_core_path= __DIR__ . '/../../core/';
+    $modx_core_path = __DIR__ . '/../../core/';
     define('MODX_CORE_PATH', $modx_core_path);
 }
 if (!defined('MODX_PROCESSORS_PATH')) {
-    $modx_processors_path= __DIR__ . '/../../core/src/Revolution/Processors/';
+    $modx_processors_path = __DIR__ . '/../../core/src/Revolution/Processors/';
     define('MODX_PROCESSORS_PATH', $modx_processors_path);
 }
 if (!defined('MODX_CONNECTORS_PATH')) {
-    $modx_connectors_path= __DIR__ . '/../../connectors/';
-    $modx_connectors_url= '/connectors/';
+    $modx_connectors_path = __DIR__ . '/../../connectors/';
+    $modx_connectors_url = '/connectors/';
     define('MODX_CONNECTORS_PATH', $modx_connectors_path);
     define('MODX_CONNECTORS_URL', $modx_connectors_url);
 }
 if (!defined('MODX_MANAGER_PATH')) {
-    $modx_manager_path= __DIR__ . '/../../manager/';
-    $modx_manager_url= '/manager/';
+    $modx_manager_path = __DIR__ . '/../../manager/';
+    $modx_manager_url = '/manager/';
     define('MODX_MANAGER_PATH', $modx_manager_path);
     define('MODX_MANAGER_URL', $modx_manager_url);
 }
 if (!defined('MODX_BASE_PATH')) {
-    $modx_base_path= __DIR__ . '/../../';
-    $modx_base_url= '/';
+    $modx_base_path = __DIR__ . '/../../';
+    $modx_base_url = '/';
     define('MODX_BASE_PATH', $modx_base_path);
     define('MODX_BASE_URL', $modx_base_url);
 }
@@ -60,7 +60,7 @@ if (!defined('MODX_URL_SCHEME')) {
 }
 if (!defined('MODX_HTTP_HOST')) {
     if(defined('PHP_SAPI') && (PHP_SAPI == "cli" || PHP_SAPI == "embed")) {
-        $http_host='dk-lok';
+        $http_host = 'dk-lok';
         define('MODX_HTTP_HOST', $http_host);
     } else {
         $http_host = array_key_exists('HTTP_HOST', $_SERVER) ? parse_url($url_scheme . $_SERVER['HTTP_HOST'], PHP_URL_HOST) : 'dk-lok';
@@ -70,12 +70,12 @@ if (!defined('MODX_HTTP_HOST')) {
     }
 }
 if (!defined('MODX_SITE_URL')) {
-    $site_url= $url_scheme . $http_host . MODX_BASE_URL;
+    $site_url = $url_scheme . $http_host . MODX_BASE_URL;
     define('MODX_SITE_URL', $site_url);
 }
 if (!defined('MODX_ASSETS_PATH')) {
-    $modx_assets_path= __DIR__ . '/../../assets/';
-    $modx_assets_url= '/assets/';
+    $modx_assets_path = __DIR__ . '/../../assets/';
+    $modx_assets_url = '/assets/';
     define('MODX_ASSETS_PATH', $modx_assets_path);
     define('MODX_ASSETS_URL', $modx_assets_url);
 }
