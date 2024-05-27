b52b1243ab0eaf264a49aa1c11a30ea6cf5c7687
Grassdor <rrr345@mail.ru>
2024-05-27 17:13:02 +0300

    hotfix image script

--- assets/base/snippets/product_getimage.php
+++ assets/base/snippets/product_getimage.php
@@ -1,29 +1,29 @@
 <?php
 global $modx;
 $parent = $scriptProperties['parent'];
-$sql = "SELECT id, template FROM modx_site_content WHERE id = :parent LIMIT 1";
+$sql = "SELECT id, content FROM modx_site_content WHERE id = :parent LIMIT 1";
 $stmt = $modx->prepare($sql);
 $stmt->execute(['parent' => $parent]);
 $directoryArr = $stmt->fetch(PDO::FETCH_ASSOC);
-$template = $directoryArr['template'];
+$content = $directoryArr['content'];
 
 
 
-    while ($template !== 2) {
-        $sql = "SELECT id, template FROM modx_site_content WHERE parent = :parent LIMIT 1";
-        $stmt = $modx->prepare($sql);
-
-        $stmt->execute(['parent' => $parent]);
-        $directoryArr = $stmt->fetch(PDO::FETCH_ASSOC);
-        $template = $directoryArr['template'];
-        $parent = $directoryArr['id'];
-    }
-    $sql = "SELECT `value` FROM modx_site_tmplvar_contentvalues WHERE contentid = :id AND tmplvarid = 5 LIMIT 1";
+while ($content !== '[[$product_page]]') {
+    $sql = "SELECT id, content FROM modx_site_content WHERE parent = :parent LIMIT 1";
     $stmt = $modx->prepare($sql);
 
-    $stmt->execute(['id' => $parent]);
+    $stmt->execute(['parent' => $parent]);
     $directoryArr = $stmt->fetch(PDO::FETCH_ASSOC);
-    echo '<img alt="" src="' . $directoryArr['value'] .'">';
+    $content = $directoryArr['content'];
+    $parent = $directoryArr['id'];
+}
+$sql = "SELECT `value` FROM modx_site_tmplvar_contentvalues WHERE contentid = :id AND tmplvarid = 5 LIMIT 1";
+$stmt = $modx->prepare($sql);
+
+$stmt->execute(['id' => $parent]);
+$directoryArr = $stmt->fetch(PDO::FETCH_ASSOC);
+echo '<img alt="" src="' . $directoryArr['value'] .'">';
 //    echo $modx->getChunk('subsection_setimage', [
 //        'value' => $directoryArr['value'],
 //        'alttext' => $alt,
